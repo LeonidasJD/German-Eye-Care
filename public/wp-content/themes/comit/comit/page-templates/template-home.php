e92ddd6135200823139f7648bc48c272ad77bbf4
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-25 17:30:47 +0200

    uploaded plugin

--- public/wp-content/themes/comit/comit/page-templates/template-home.php
+++ public/wp-content/themes/comit/comit/page-templates/template-home.php
@@ -25,8 +25,12 @@ get_header();
                     <p>Rufen Sie noch heute an oder kontaktieren Sie uns per <a href="#">E-Mail</a> oder Kontaktformular.Wir machen Ihre Augen zu unserer Herzenssache!</p>
                 </div>
                 <div class="main-form">
-                    <h2>form placeholder</h2>
                     
+                    <div class="form-wrapper">
+                        <h2>Termin Buchen</h2>
+                    <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2 ) ); ?>
+                    
+                    </div>
                   
                 </div>
             </div>
@@ -35,3 +39,118 @@ get_header();
  </div>
  <!-- HERO SECTION END -->
 <?php get_footer(); ?>
+
+
+
+<script>
+
+    // DODAVANJE ELEMENATA U PLUGIN FORMU START
+
+    
+    document.addEventListener('DOMContentLoaded', (event) => {
+    // Kreiranje prvog div elementa sa klasom "paralel-fields"
+    var firstParallelFieldsDiv = document.createElement('div');
+    firstParallelFieldsDiv.className = 'paralel-fields';
+
+    // Pronalazak postojećeg div elementa sa ID-jem "frm_field_8_container"
+    var existingDiv = document.getElementById('frm_field_8_container');
+
+    // Umetanje prvog paralel-fields div elementa ispod postojećeg div elementa
+    existingDiv.parentNode.insertBefore(firstParallelFieldsDiv, existingDiv.nextSibling);
+
+    // Pronalazak elemenata sa ID-jevima "frm_field_9_container" i "frm_field_11_container"
+    var field9 = document.getElementById('frm_field_9_container');
+    var field11 = document.getElementById('frm_field_11_container');
+
+    // Premestanje elemenata unutar prvog paralel-fields div elementa
+    firstParallelFieldsDiv.appendChild(field9);
+
+    // Kreiranje novog div elementa sa klasom "form-divider"
+    var dividerDiv1 = document.createElement('div');
+    dividerDiv1.className = 'form-divider';
+
+    // Kreiranje slike unutar div elementa sa klasom "form-divider"
+    var img1 = document.createElement('img');
+    img1.src = '/wp-content/uploads/2024/06/vector_21.webp';
+    dividerDiv1.appendChild(img1);
+
+    // Umetanje "form-divider" div elementa između "frm_field_9_container" i "frm_field_11_container"
+    firstParallelFieldsDiv.appendChild(dividerDiv1);
+
+    // Premestanje "frm_field_11_container" unutar prvog paralel-fields div elementa
+    firstParallelFieldsDiv.appendChild(field11);
+
+    // Kreiranje drugog div elementa sa klasom "paralel-fields"
+    var secondParallelFieldsDiv = document.createElement('div');
+    secondParallelFieldsDiv.className = 'paralel-fields';
+
+    // Umetanje drugog paralel-fields div elementa ispod prvog paralel-fields div elementa
+    firstParallelFieldsDiv.parentNode.insertBefore(secondParallelFieldsDiv, firstParallelFieldsDiv.nextSibling);
+
+    // Pronalazak elemenata sa ID-jevima "frm_field_12_container" i "frm_field_13_container"
+    var field12 = document.getElementById('frm_field_12_container');
+    var field13 = document.getElementById('frm_field_13_container');
+
+    // Premestanje "frm_field_12_container" unutar drugog paralel-fields div elementa
+    secondParallelFieldsDiv.appendChild(field12);
+
+    // Kreiranje drugog div elementa sa klasom "form-divider"
+    var dividerDiv2 = document.createElement('div');
+    dividerDiv2.className = 'form-divider';
+
+    // Kreiranje slike unutar div elementa sa klasom "form-divider"
+    var img2 = document.createElement('img');
+    img2.src = '/wp-content/uploads/2024/06/vector_21.webp';
+    dividerDiv2.appendChild(img2);
+
+    // Umetanje drugog "form-divider" div elementa između "frm_field_12_container" i "frm_field_13_container"
+    secondParallelFieldsDiv.appendChild(dividerDiv2);
+
+    // Premestanje "frm_field_13_container" unutar drugog paralel-fields div elementa
+    secondParallelFieldsDiv.appendChild(field13);
+});
+    // DODAVANJE ELEMENATA U PLUGIN FORMU END
+
+    // KADA ODABEREMO POLJE DROPWDONU U FORMI MENJA SE FONT START
+    const dropdownServiceHome = document.getElementById('field_coxqx');
+
+    dropdownServiceHome.addEventListener('change', function() {
+  if (dropdownServiceHome.value === "") {
+    dropdownServiceHome.classList.remove('change-select');
+  } else {
+    dropdownServiceHome.classList.add('change-select');
+  }
+});
+
+
+const dropdownSandortHome = document.getElementById('field_3cari');
+
+dropdownSandortHome.addEventListener('change', function() {
+if (dropdownSandortHome.value === "") {
+    dropdownSandortHome.classList.remove('change-select');
+} else {
+    dropdownSandortHome.classList.add('change-select');
+}
+});
+
+const hourHome = document.getElementById('field_lddwq_H');
+
+hourHome.addEventListener('change', function() {
+if (hourHome.value === "") {
+    hourHome.classList.remove('change-select');
+} else {
+    hourHome.classList.add('change-select');
+}
+});
+
+const minuteHome = document.getElementById('field_lddwq_m');
+
+minuteHome.addEventListener('change', function() {
+if (minuteHome.value === "") {
+    minuteHome.classList.remove('change-select');
+} else {
+    minuteHome.classList.add('change-select');
+}
+});
+    // KADA ODABEREMO POLJE DROPWDONU U FORMI MENJA SE FONT END
+</script>
