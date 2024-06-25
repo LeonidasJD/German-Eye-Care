222d76eff75562659a575fff3de34b71dc498eb1
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-25 11:45:45 +0200

    updated plugin and added images

--- public/wp-content/themes/comit/comit/page-templates/template-home.php
+++ public/wp-content/themes/comit/comit/page-templates/template-home.php
@@ -2,5 +2,36 @@
 //Template Name: Home
 get_header();
 ?>
-<div class="test-home"><h1>test homepage</h1></div>
+<!-- HERO SECTION START -->
+ <div class="hero-container">
+    <div class="container-14">
+        <section class="hero-section-wrapper">
+            <div class="hero-section-underwrapper">
+                <h1>GermanEyeCare:<br>
+                Kompetenz für Sie!</h1>
+            </div>
+        </section>
+    </div>
+ </div>
+ <div class="text-and-form-container">
+    <div class="container-14">
+        <section class="text-and-form-wrapper">
+            <div class="text-and-form-underwrapper">
+                <div class="main-text">
+                    <h2>Was passiert, wenn sich das <br> Sehen verschlechtert?</h2>
+                    <p>In solchen Fällen sind eine genaue Ursachenforschung, Therapie und fokussierte Versorgung notwendig. Das gelingt am besten mit einem Team von Spezialisten, die ihr Handwerk dank breit gefächerter Ausbildung, jahrelanger Erfahrungen und Empathie bestens beherrschen.</p>
+                    <p>Das Augenfürsorge-Netzwerk "GermanEyeCare" ist entstanden, um genau diese fachübergreifende Zusammenarbeit für Ihre Augen und Augengesundheit entstehen zulassen und somit einen nachhaltigen und wirksamen Beitrag zur Augengesundheit zu leisten.</p>
+                    <p>Bei uns können Sie sich nicht nur einen Überblick zum Thema Auge und Gesundheit verschaffen, sondern sich auch jederzeit an unsere Experten wenden, sollten Sie weitere Fragen haben. Wir beraten Sie gern und bieten Ihnen eine perfekt auf Sie zugeschnittene Versorgung. Ein optimal aufeinander abgestimmtes Expertenteam wartet auf Sie.</p>
+                    <p>Rufen Sie noch heute an oder kontaktieren Sie uns per <a href="#">E-Mail</a> oder Kontaktformular.Wir machen Ihre Augen zu unserer Herzenssache!</p>
+                </div>
+                <div class="main-form">
+                    <h2>form placeholder</h2>
+                    
+                  
+                </div>
+            </div>
+        </section>
+    </div>
+ </div>
+ <!-- HERO SECTION END -->
 <?php get_footer(); ?>
