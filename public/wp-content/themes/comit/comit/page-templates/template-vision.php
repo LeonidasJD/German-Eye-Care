fe35289588ae289fa52ce87ed09c02361415e9d9
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-27 15:01:22 +0200

    created template vision

--- public/wp-content/themes/comit/comit/page-templates/template-vision.php
+++ public/wp-content/themes/comit/comit/page-templates/template-vision.php
@@ -2,5 +2,143 @@
 //Template Name: Vision
 get_header();
 ?>
+<!-- HERO SECTION START -->
+<div class="vision-hero-container">
+    <div class="container-14">
+        <div class="vision-hero-wrapper">
+            <div class="vision-hero-underwrapper">
+                <h1>A world of cooperation.</h1>
+                <h2>together - competent - sustainable</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- HERO SECTION END -->
 
+<!-- OUR STORY SECTION START -->
+ <div class="our-story-container">
+    <div class="container-14">
+        <div class="our-story-wrapper">
+            <div class="our-story-underwrapper">
+                <div class="small-text-wrapper">
+                    <div class="small-dot"><img src="/wp-content/uploads/2024/06/green-dot.webp" alt=""></div>
+                    <div class="small-text"><p>Our Story</p></div>
+                </div>
+                <div class="heading-and-paragraf">
+                <h1>What is GermanEyeCare for?
+                </h1>
+                <p>We only know about the existence of an object because we can see, hear, taste, smell or feel it. The sense of sight provides us up to 80% all information from the 
+                outside world. </p>
+                <div class="line-divider"></div>
+                <div class="rest-paragraphs-wrapper">
+                <p>The eyes are among the best  blooded organs in the entire body. Your blood supply, which takes place via your own artery, is closely linked to the blood supply to the brain. Furthermore, the eyes are involved above average in the pairs of brain nerves. A total of six of the twelve pairs of cranial nerves serve to innervate the eyes.</p>
+                <p>These and many other anatomical details, such as the enormous metabolic rate and the high sensory cell density, are influenced by the physiological, growth and aging-related changes but also by external factors. Whether these changes have a normal or pathological character is crucial for maintaining eye health and its influence on vision.If you look at the eyes in the overall human system, there are many aspects for one holistic view, such as diabetes mellitus and eye or healthy vision in the workplace.</p>
+                <p>Optometric screening of the eyes often gives the first diagnostic indications of a general illness or an abnormal development of vision. Then there are no new glasses, but the Mediation to the respective specialist or healthcare provider.So far, the customer has had to strive for the various options for vision deterioration and its clarification.GermanEyeCare would like to close this gap with its interdisciplinary cooperation and make a lasting contribution to holistic eye health. That means: at GermanEyeCare</p>
+                <p>let's take the time for a comprehensive and holistic initial consultation. Should we provide diagnostic information on a general illness or similar Recognize, we mediate in consultation with the patient, to the responsible specialist or healthcare provider. </p>
+                </div>
+               
+                </div>
+               
+            </div>
+        </div>
+    </div>
+ </div>
+ <!-- OUR STORY SECTION END -->
+
+ <!-- CHECK OUR BLOG SECTION START -->
+  <div class="check-section-container">
+    <div class="container-14">
+        <div class="check-section-wrapper">
+            <div class="check-section-underwrapper">
+                <div class="check-data">
+                <h2>Get an insight into our blogs</h2>
+                <p>Lorem ipsum dolor sit amet consectetur. Adipiscing elit faucibus elit lacinia laoreet molestie. Nunc orci venenatis </p>
+                <a href="#">Check our blog</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    
+  </div>
+  <!-- CHECK OUR BLOG SECTION END -->
+
+  <!-- EXPRETISE IS KEY SECTION START -->
+   <div class="expertise-section-container">
+    <div class="container-14">
+        <div class="expertise-section-wrapper">
+            <div class="expertise-section-underwrapper">
+                <h2>Expertise is the key...<img src="/wp-content/uploads/2024/06/play-button-icon.webp" ></h2>
+            </div>
+        </div>
+    </div>
+   </div>
+   <!-- EXPRETISE IS KEY SECTION END -->
+
+   <!-- OUR PARTNERS SECTION START -->
+    <div class="our-partners-container">
+        <div class="container-14">
+            <div class="our-partners-wrapper">
+                <div class="our-partners-underwrapper">
+                    <div class="heading-and-text">
+                    <h2>Our partners</h2>
+                    <p>In the belief that holistic eye care requires interdisciplinary cooperation, Randy Freitag has built up a comprehensive national network of health partners.  </p>
+                    </div>
+                    <div class="two-banners-wrapper">
+                        <div class="main-banners">
+                        <div class="single-banner banner-with-border">
+                            <h2>ABW</h2>
+                            <p>For example, if an accommodative inward squint is diagnosed in a child, further treatment by an ophthalmologist / orthoptist is necessary.</p>
+                            <img src="/wp-content/uploads/2024/06/abw-logo.webp" >
+                        </div>
+                        <div class="single-banner banner-without-border">
+                            <h2>BSVSB</h2>
+                            <p>Whereas for a blind or visually impaired person, it is advisable to put them in touch with a social institution such as the
+                            South Baden Association for the Blind and Visually Impaired in order to possibly apply for blindness benefits. </p>
+                            <img src="/wp-content/uploads/2024/06/bsvsb-logo.webp" >
+                        </div>
+                        </div>
+                        <div class="under-black-banner-wrapper">
+                        <div class="under-black-banner">
+                            <div class="under-black-baner-img"><img src="/wp-content/uploads/2024/06/book-logo-e1719487886497.webp" ></div>
+                            <div class="under-black-banner-paragraph"><p>We can also arrange <span>internships</span> for students with various health care providers for optimal training and further education.</p></div>
+                        </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- OUR PARTNERS SECTION END -->
+
+    <!-- HELTHCARE PARTNERS START-->
+     <div class="helthcare-container">
+        <div class="container-14">
+            <div class="helthcare-wrapper">
+                <div class="helthcare-underwrapper">
+                    <h2>Here is an excerpt from our healthcare partners:</h2>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="marquee-content">
+                        <marquee scrollamount="15"> 
+                            Endocrinologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Diabetologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Oncologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Radiologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Neurologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Orthoptists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            HoffmannOptik <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Eye care center <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Optometrists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Vision schools <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                            Cardiologists <img src="/wp-content/uploads/2024/06/green-dot.webp">
+                        </marquee>
+                        
+     </div>
+     </div>
+     <!-- HELTHCARE PARTNERS END -->
 <?php get_footer(); ?>
