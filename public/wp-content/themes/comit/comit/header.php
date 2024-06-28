5fac29c5b08f734ab9ba8630b39f5cd50431e4f4
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-28 16:32:29 +0200

    added lib for scroll map

--- public/wp-content/themes/comit/comit/header.php
+++ public/wp-content/themes/comit/comit/header.php
@@ -22,6 +22,9 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 	 <!-- FONTS END -->
+	<!-- LIB FOR MOUSE SCROLL ON MAP START -->
+	 <script src="https://unpkg.com/leaflet-mouseposition/Leaflet.MousePosition.min.js"></script>
+	 <!-- LIB FOR MOUSE SCROLL ON MAP START -->
 	<?php wp_head(); ?>
 </head>
 
