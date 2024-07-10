34a3c77ed1978dd2c7aa8db074e752eaa8a1bd70
MILAN DJORDJEVIC <milandjordjevic633@gmail.com>
2024-07-10 12:20:39 +0200

    changed style

--- public/wp-content/themes/comit/comit/page-templates/template-home.php
+++ public/wp-content/themes/comit/comit/page-templates/template-home.php
@@ -25,7 +25,7 @@ get_header();
                     <p>In solchen Fällen sind eine genaue Ursachenforschung, Therapie und fokussierte Versorgung notwendig. Das gelingt am besten mit einem Team von Spezialisten, die ihr Handwerk dank breit gefächerter Ausbildung, jahrelanger Erfahrungen und Empathie bestens beherrschen.</p>
                     <p>Das Augenfürsorge-Netzwerk "GermanEyeCare" ist entstanden, um genau diese fachübergreifende Zusammenarbeit für Ihre Augen und Augengesundheit entstehen zulassen und somit einen nachhaltigen und wirksamen Beitrag zur Augengesundheit zu leisten.</p>
                     <p>Bei uns können Sie sich nicht nur einen Überblick zum Thema Auge und Gesundheit verschaffen, sondern sich auch jederzeit an unsere Experten wenden, sollten Sie weitere Fragen haben. Wir beraten Sie gern und bieten Ihnen eine perfekt auf Sie zugeschnittene Versorgung. Ein optimal aufeinander abgestimmtes Expertenteam wartet auf Sie.</p>
-                    <p>Rufen Sie noch heute an oder kontaktieren Sie uns per <a href="#">E-Mail</a> oder Kontaktformular.Wir machen Ihre Augen zu unserer Herzenssache!</p>
+                    <p>Rufen Sie noch heute an oder kontaktieren Sie uns per <a href="#">E-Mail</a> oder Kontaktformular . Wir machen Ihre Augen zu unserer Herzenssache!</p>
                 </div>
                 <div class="main-form">
                     
