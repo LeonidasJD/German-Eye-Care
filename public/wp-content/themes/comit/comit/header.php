0324fa4d76b710d2efe1e09cd38c76c170ae347d
MILAN DJORDJEVIC <milandjordjevic633@gmail.com>
2024-07-18 16:00:42 +0200

    updated header

--- public/wp-content/themes/comit/comit/header.php
+++ public/wp-content/themes/comit/comit/header.php
@@ -17,6 +17,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<script src="https://www.google.com/recaptcha/api.js"></script>
+	<!-- RECAPTCHA V3 -->
+
 	<!-- FONTS START -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
